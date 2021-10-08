f53ee1571f63e6a80a5e3d98af48258a14f10ff5
Alan Fabian Herrera <alanfg120@gmail.com>
2021-10-08 09:25:31 -0500

    delete producto getproductos

--- app/componets/productos/data/producto.repositorio.ts
+++ app/componets/productos/data/producto.repositorio.ts
@@ -1,51 +1,69 @@
 import { UploadedFile } from "express-fileupload";
-import { ErrorResponse } from "../../../interfaces/Error.enum";
+import {
+  ErrorHttpResponse,
+  ErrorResponse,
+} from "../../../interfaces/Error.enum";
 import { Producto } from "../models/producto.model";
-
-
-
-
+import fs from "fs-extra";
 export class ProductoRepositorio {
-    
-  async getProductos(): Promise<Producto[] | ErrorResponse> {
+  async getProductos(): Promise<Producto[] | ErrorHttpResponse> {
     try {
-      const Productos = await Producto.findAll({});
+      const Productos = await Producto.findAll();
       return Productos;
     } catch (error) {
       console.log(error);
-      return ErrorResponse.errorDataBase;
+      return { error: ErrorResponse.errorDataBase };
     }
   }
-  
-  async addProducto(caja: any,file: UploadedFile): Promise<Producto | ErrorResponse> {
+
+  async addProducto(
+    caja: any,
+    file: UploadedFile
+  ): Promise<Producto | ErrorHttpResponse> {
     try {
-      const newCaja = await Producto.create({ ...caja });
-      await file.mv(`${process.env.PRODUCTOS_FOLDER}/${newCaja.id}.jpg`)
-      return newCaja;
+      const newProducto = await Producto.create({ ...caja });
+      await file.mv(`${process.env.PRODUCTOS_FOLDER}/${newProducto.id}.jpg`);
+      const [rowAfect, productos] = await Producto.update(
+        { imagen: `${newProducto.id}.jpg` },
+        { where: { id: newProducto.id } }
+      );
+      if (rowAfect > 0) {
+        newProducto.imagen = `${newProducto.id}.jpg`;
+        return newProducto;
+      } else throw new Error("error al crear el producto");
     } catch (error) {
       console.log(error);
-      return ErrorResponse.errorDataBase;
+      return { error: ErrorResponse.errorDataBase };
     }
   }
-  async updateProducto(id: number, Producto: any): Promise<boolean | ErrorResponse> {
+  async updateProducto(
+    id: number,
+    producto: any,
+    file?: UploadedFile
+  ): Promise<boolean | ErrorHttpResponse> {
     try {
-      const [row, cajas] = await Producto.update({ ...Producto }, { where: { id } });
-      if(row> 0) return true;
-      else return false
+      const [row, cajas] = await Producto.update(
+        { ...producto },
+        { where: { id } }
+      );
+      if (file) await file.mv(`${process.env.PRODUCTOS_FOLDER}/${id}.jpg`);
+      if (row > 0) return true;
+      else return false;
     } catch (error) {
       console.log(error);
-      return ErrorResponse.errorDataBase;
+      return { error: ErrorResponse.errorDataBase };
     }
   }
-  async deleteProducto(id: number): Promise<boolean | ErrorResponse> {
+  async deleteProducto(id: number): Promise<boolean | ErrorHttpResponse> {
     try {
-      const row  = await Producto.destroy({ where: { id } });
-      if(row> 0) return true;
-      else return false
+      const row = await Producto.destroy({ where: { id } });
+      if (row > 0) {
+        await fs.remove(`${process.env.PRODUCTOS_FOLDER}/${id}.jpg`);
+        return true;
+      } else return false;
     } catch (error) {
       console.log(error);
-      return ErrorResponse.errorDataBase;
+      return { error: ErrorResponse.errorDataBase };
     }
   }
 }
- 
